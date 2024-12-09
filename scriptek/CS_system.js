769c291518d780f79a170f73efd35e3688b84402
lipotjoci <unconfigured@null.spigotmc.org>
2024-12-09 23:07:29 +0100

    globális coin és highscore rendszer elkezdve

---
+++ scriptek/CS_system.js
@@ -0,0 +1,64 @@
+let globalCoinCount = 0;
+let globalHighscore = 0;
+
+if(getCookie("coinCount") != "") {
+    globalCoinCount = Number(getCookie("coinCount"));
+    console.log("Remembered coin count! ", globalCoinCount)
+} else setCookie("coinCount", 0);
+
+if(getCookie("savedHighscore") != "") {
+    globalHighscore = Number(getCookie("savedHighscore"));
+    console.log("Remembered highscore! ", globalHighscore)
+} else setCookie("savedHighscore", 0);
+
+function saveCoins() {
+    setCookie("coinCount", globalCoinCount);
+}
+
+function saveHighscore() {
+    setCookie("savedHighscore", globalHighscore);
+}
+
+function setCookie(cname, cvalue, exdays = 1000) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+function sethighScore(newHG) {
+    if (globalHighscore < newHG) {
+        globalHighscore = newHG;
+        saveHighscore();
+    }
+}
+
+function addCoins(amount) {
+    globalCoinCount += amount;
+    saveCoins();
+}
+
+function subCoins(amount) {
+    if (globalCoinCount - amount < 0) return false;
+    else { 
+        globalCoinCount -= amount
+        saveCoins();
+        return true;
+    }
+}
